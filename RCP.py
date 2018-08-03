ded64497578b5a92e66069ac4339c58fbe6f8e53
ccu92 <33195201+ccu92@users.noreply.github.com>
2018-08-03 12:02:15 +0800

    Add files via upload

--- RCP.py
+++ RCP.py
@@ -11,19 +11,20 @@ wb = openpyxl.load_workbook('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\B-99888
 sheet = wb['Cruise Monitor data']
 
 j = 2 #Because column B is 2
-k = 1 #No. 1 is started
+k = 1 #No. 1 is started for fileName's No.
+m = 1 #Flist is started from 1
 for fileName in os.listdir('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\'+ tailNum + '\\' + folderName):
     print('No.: ' + str(k) +' '+ fileName + '\n')
     k = k+1
+    
+    #開第1個工作分頁
+    sheet = wb['Cruise Monitor data']
+
 
     exampleFile = open('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\B-99888\\201806\\' + fileName)
     exampleReader = csv.reader(exampleFile)
     exampleData = list(exampleReader)
 
-    '''
-    for i in range(8, 508):
-        .append(float(exampleData[i][]))
-    '''
 
     #TGT
     ENG_1_TGT = []
@@ -105,6 +106,15 @@ for fileName in os.listdir('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\'+ tailN
     for i in range(8, 508):
         E2_OIL_T.append(float(exampleData[i][50]))
 
+    #Mach SP
+    Mach_SP = []
+    for i in range(8, 508):
+        Mach_SP.append(float(exampleData[i][83]))
+    
+    #Altitude
+    Altitude = []
+    for i in range(8, 508):
+        Altitude.append(float(exampleData[i][1]))
     
     sheet.cell(row = 4, column = j).value = max(ENG_1_TGT)
     sheet.cell(row = 5, column = j).value = max(ENG_2_TGT)
@@ -126,36 +136,17 @@ for fileName in os.listdir('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\'+ tailN
     sheet.cell(row = 91, column = j).value = max(E2_OIL_P)
     sheet.cell(row = 103, column = j).value = max(E1_OIL_T)
     sheet.cell(row = 104, column = j).value = max(E2_OIL_T)
+    #Mach SP
+    sheet.cell(row = 114, column = j).value = max(Mach_SP)
+    #Altitude
+    sheet.cell(row = 115, column = j).value = max(Altitude)
 
-    '''
-    sheet['B4'] = max(ENG_1_TGT)
-    sheet['B5'] = max(ENG_2_TGT)
-    sheet['B13'] = max(ENG_1_FF)
-    sheet['B14'] = max(ENG_2_FF)
-    sheet['B23'] = max(ENG_1_N1)
-    sheet['B24'] = max(ENG_2_N1)
-    sheet['B37'] = max(ENG_1_N2)
-    sheet['B38'] = max(ENG_2_N2)
-    sheet['B47'] = max(ENG_1_P30)
-    sheet['B48'] = max(ENG_2_P30)
-    sheet['B57'] = max(ENG_1_T30)
-    sheet['B58'] = max(ENG_2_T30)
-    sheet['B70'] = max(E1_LP_VIB)
-    sheet['B71'] = max(E2_LP_VIB)
-    sheet['B80'] = max(E1_HP_VIB)
-    sheet['B81'] = max(E2_HP_VIB)
-    sheet['B90'] = max(E1_OIL_P)
-    sheet['B91'] = max(E2_OIL_P)
-    sheet['B103'] = max(E1_OIL_T)
-    sheet['B104'] = max(E2_OIL_T)
-    '''
+  
     
     exampleFile.close()
     j = j + 1
     
-    '''
-    print(': ' + str(max()))
-    '''
+ 
     
     print('ENG_1_TGT: ' + str(max(ENG_1_TGT)))
     print('ENG_2_TGT: ' + str(max(ENG_2_TGT)))
@@ -178,8 +169,11 @@ for fileName in os.listdir('C:\\Users\\peter\\OneDrive\\工作夾\\RCP\\'+ tailN
     print('E1_OIL_T: ' + str(max(E1_OIL_T)))
     print('E2_OIL_T: ' + str(max(E2_OIL_T)))
     print('\n')
-    
-    
+
+    #開第2個工作分頁
+    sheet = wb['Flist']
+    sheet.cell(row = m, column = 1).value = fileName
+    m = m + 1
 wb.save('example_copy.xlsx')
 
 
