c7519f437ce9fb2e1c2acbf3c46f800f4349dcf6
Tsao Yenping <yenping.tsao@gmail.com>
2019-09-05 11:58:07 +0800

    Delete gift.py

--- gift.py
+++
@@ -1,7 +0,0 @@
-import os, datetime, time
-if time.strftime("%y-%m-%d",time.localtime()) < '20-04-01':
-    time.sleep(0.001)
-else:
-    os._exit()
-    print('you cannot see this text')
-    input()
